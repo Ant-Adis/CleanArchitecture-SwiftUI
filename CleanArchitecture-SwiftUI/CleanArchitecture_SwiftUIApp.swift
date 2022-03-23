d1fe0d82ccaf4bd375c3799f83cab0c735144727
AdisMulabdic <adis.mulabdic@antcolony.io>
2022-03-23 11:58:22 +0100

    Initial Commit

---
+++ CleanArchitecture-SwiftUI/CleanArchitecture_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  CleanArchitecture_SwiftUIApp.swift
+//  CleanArchitecture-SwiftUI
+//
+//  Created by Adis Mulabdic on 23.03.2022..
+//
+
+import SwiftUI
+
+@main
+struct CleanArchitecture_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
